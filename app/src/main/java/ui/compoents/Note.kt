fad47a0021f555220fa16c0b677448cb2ae1f542
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-04-25 15:26:58 +0300

    add Note.kt

---
+++ app/src/main/java/ui/compoents/Note.kt
@@ -0,0 +1,10 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable fun Note() {
+}
+    @Preview
+    @Composable
+    private fun NotePreview() {
+    Note()
+}
